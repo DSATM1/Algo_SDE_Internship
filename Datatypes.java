69f7a9b50eae69dd19df7caf93e1beb8e363c56e
Suraj <surajsppokale@gmail.com>
2026-08-24 17:47:10 +0530

    Maths

--- Datatypes.java
+++ Datatypes.java
@@ -1,6 +1,10 @@
 public class Datatypes {
     public static void main(String [] args)
     {
+        int a1 = 12;
+        int a2 = 32;
+        int add = a1 + a2;
+        System.out.println(add); 
         System.out.println("Hello Boyyyysssss");
     }
 }
