a81ebd32f90c258f27d71c63cd59bf8c95da9938
Suraj <surajsppokale@gmail.com>
2026-08-25 16:03:16 +0530

    Average and Percentage

--- Day6_Activity.java
+++ Day6_Activity.java
@@ -19,12 +19,28 @@ public class Day6_Activity
         // byte breadth = 20;
         // System.out.println("Area of Rectangle " + length * breadth);
         
+        // Temperature
         // byte Faren = 25  ;
         // float Cel = 12;
         // System.out.println("C to F is " + (Cel * 9/5) + 32 + Faren);
         // System.out.println("F to C is " + (Faren - 32) * 5/9 + Cel);
 
+
+        byte s1 = 35;
+        byte s2 = 78;
+        byte s3 = 90;
+        byte s4 = 100;
+        byte s5 = 50;
         
+        int Total = s1 + s2 + s3 + s4 + s5;
+
+        double Avg = (s1 + s2 + s3 + s4 + s5)/5 ;
+
+        float Perc = ((float)Total/500)*100f;
+
+        System.out.println(Total);
+        System.out.println(Avg);
+        System.out.println(Perc);
 
 
     }
