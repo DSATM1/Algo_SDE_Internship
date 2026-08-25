77d6a6c6de1e226446b7790e3182c86d1100a823
Suraj <surajsppokale@gmail.com>
2026-08-25 11:34:13 +0530

    initial value of datatypes

--- Day_6.java
+++ Day_6.java
@@ -1,5 +1,13 @@
 public class Day_6 
-{
+{    
+        // static byte a;
+        // static int num;
+        // static short s;
+        // static long l;
+        // static float f;
+        // static char c;
+        // static double d;
+        // static boolean b;
     public static void main(String[] args)
     {
 
@@ -26,16 +34,29 @@ public class Day_6
         // System.out.println(age);
 
         // Integral type represented in different types
-        byte b1 = 10; // Decimal Value 
-        byte b2 = 0b1010; // Binary Value 
-        byte b3 = 012; // Octal Value
-        byte b4 = 0xA; // Hexadecimal Value
+        // byte b1 = 10; // Decimal Value 
+        // byte b2 = 0b1010; // Binary Value 
+        // byte b3 = 012; // Octal Value
+        // byte b4 = 0xA; // Hexadecimal Value
 
-        System.out.println(b1);
-        System.out.println(b2);
-        System.out.println(b3);
-        System.out.println(b4);
-        System.out.println(b1 + " " + b2 + " " + b3 + " " + b4);
+        // System.out.println(b1);
+        // System.out.println(b2);
+        // System.out.println(b3);
+        // System.out.println(b4);
+        // System.out.println(b1 + " " + b2 + " " + b3 + " " + b4);
+
+        
+        // System.out.println(" byte        : " + a);
+        // System.out.println(" int         : " + num);
+        // System.out.println(" short       : " + s);
+        // System.out.println(" long        : " + l);
+        // System.out.println(" float       : " + f);
+        // System.out.println(" char        : " + c);
+        // System.out.println(" double      : " + d);
+        // System.out.println(" boolean     : " + b);
+
+
+
 
 
 
