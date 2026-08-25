1aeac09cf9b5376a6cc0c4e408a7c2319237dff5
Suraj <surajsppokale@gmail.com>
2026-08-25 10:55:11 +0530

    Age Program Variable Concept

--- Day_6.java
+++ Day_6.java
@@ -2,6 +2,27 @@ public class Day_6
 {
     public static void main(String[] args)
     {
-        System.out.println("");
+
+        
+        /*why variable even though we can print values directly 
+        bcz for future use if i want change data then i can't 
+        change all the 100 entries if there are 100 entries so we need variable 
+        */
+        // System.out.println(25);
+        // System.out.println(3.14);
+        // System.out.println(3.143224);
+        // System.out.println(true);
+        // // System.out.println('Ai');
+        // System.out.println("Suraj");
+        // // System.out.println(null);
+
+
+
+        byte age = 26;
+        System.out.println(age);
+        System.out.println(age);
+        System.out.println(age);
+        System.out.println(age);
+        System.out.println(age);
     }
 }
