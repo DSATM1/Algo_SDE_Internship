be65b3892a00f657a88ef76248ff67023b079476
Suraj <surajsppokale@gmail.com>
2026-08-25 13:57:21 +0530

    Area of Circle

--- Day6_Activity.java
+++ Day6_Activity.java
@@ -2,10 +2,18 @@ public class Day6_Activity
 {
     public static void main(String [] args)
     {
-        for (int i=0x0C80; i<= 0x0CFF; i++)
-        {
-            System.out.println((char)i);
-        }
+        // ASCII Code in Kannada --------->>>>>>>>
+        // for (int i=0x0C80; i<= 0x0CFF; i++)
+        // {
+        //     System.out.println((char)i);
+        // }
+
+        // Circum and area of a circle --------->>>>>>
+        // byte radius = 10;
+        // double pie = 3.142;
+        // System.out.println("Circumfarence of a Circle : " + 2 * pie * radius);
+        // System.out.println("Area of a Circle : " + pie * radius * radius);
+
         
     }
 }
