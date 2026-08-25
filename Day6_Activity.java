01081a2722e8704c19038f52f2ddf723140edf9d
Suraj <surajsppokale@gmail.com>
2026-08-25 15:36:58 +0530

    Temperature

--- Day6_Activity.java
+++ Day6_Activity.java
@@ -14,6 +14,18 @@ public class Day6_Activity
         // System.out.println("Circumfarence of a Circle : " + 2 * pie * radius);
         // System.out.println("Area of a Circle : " + pie * radius * radius);
 
+        // Area of Rectangle 
+        // byte length = 10;
+        // byte breadth = 20;
+        // System.out.println("Area of Rectangle " + length * breadth);
         
+        // byte Faren = 25  ;
+        // float Cel = 12;
+        // System.out.println("C to F is " + (Cel * 9/5) + 32 + Faren);
+        // System.out.println("F to C is " + (Faren - 32) * 5/9 + Cel);
+
+        
+
+
     }
 }
