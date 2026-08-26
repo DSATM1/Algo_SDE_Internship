80a1ebf875b3f7b73cf262e9e10eb571163f7c0c
Suraj <surajsppokale@gmail.com>
2026-08-26 11:13:19 +0530

    Byte Inc and Dec

--- Day_7.java
+++ Day_7.java
@@ -2,12 +2,35 @@ public class Day_7
 {
     public static void main(String [] args)
     {
-        byte a = 10; // a is 10
-        byte b = 20; // b is 20
+        // byte a = 10; // a is 10
+        // byte b = 20; // b is 20
+        // b = a; // 10 goes to b so b is 10
+        // System.out.println(a); // 10
+        // System.out.println(b); // 10
 
-        b = a; // 10 goes to b so b is 10
+        // byte a = 10;
+        // byte b = 20;
+        // byte c = a;
+        // a = b;
+        // b = c;
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
 
-        System.out.println(a); // 10
-        System.out.println(b); // 10
+        // byte b = 126;
+        // System.out.println(b);
+        // b++;
+        // System.out.println(b);
+        // b++;
+        // System.out.println(b);
+
+        // byte b = Byte.MIN_VALUE;
+        // System.out.println(b);
+        // b--;
+        // System.out.println(b);
+        // b--;
+        // System.out.println(b);
+
+
     }
 }
