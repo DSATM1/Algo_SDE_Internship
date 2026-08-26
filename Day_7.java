fa1faa9ffa07cedc4fd1c5df6415f0e5d3a1c096
Suraj <surajsppokale@gmail.com>
2026-08-26 11:33:26 +0530

    Return type mismatch error checking

--- Day_7.java
+++ Day_7.java
@@ -1,7 +1,21 @@
 public class Day_7 
 {
+
+    // static int add()
+    // {
+    //     return 24;
+    // }
+
+    static String letter()
+    {
+        return "Suraj S P";
+    }
     public static void main(String [] args)
     {
+
+        System.out.println(letter());
+
+
         // byte a = 10; // a is 10
         // byte b = 20; // b is 20
         // b = a; // 10 goes to b so b is 10
@@ -33,5 +47,7 @@ public class Day_7
 
         // double a = (int)10.5; // Typecast
         // System.out.println(a); 
+
+
     }
 }
