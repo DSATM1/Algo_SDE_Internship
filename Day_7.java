f5034dfdf408bfd76dc6e25f4899e4d161e06378
Suraj <surajsppokale@gmail.com>
2026-08-26 11:02:06 +0530

    updating the b value

--- Day_7.java
+++ Day_7.java
@@ -2,6 +2,12 @@ public class Day_7
 {
     public static void main(String [] args)
     {
-        System.out.println("Hello");
+        byte a = 10; // a is 10
+        byte b = 20; // b is 20
+
+        b = a; // 10 goes to b so b is 10
+
+        System.out.println(a); // 10
+        System.out.println(b); // 10
     }
 }
