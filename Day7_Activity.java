8102885dd549e71ab0b42dc28cf58aa01b2461b0
Suraj <surajsppokale@gmail.com>
2026-08-26 13:41:37 +0530

    Integer Division and Floating Division

---
+++ Day7_Activity.java
@@ -0,0 +1,12 @@
+public class Day7_Activity 
+{
+    public static void main(String [] args)
+    {
+        // int num1 = 65;
+        // int num2 = 45;
+        // System.out.println(num1/num2);
+
+        
+
+    }
+}
