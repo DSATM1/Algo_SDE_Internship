a35cbb6f6254feb08bec7bd364cc3eb5a83de746
Suraj <surajsppokale@gmail.com>
2026-08-26 13:41:56 +0530

    Integer Division and Floating Division

--- Day7_Activity.java
+++ Day7_Activity.java
@@ -2,11 +2,34 @@ public class Day7_Activity
 {
     public static void main(String [] args)
     {
-        // int num1 = 65;
-        // int num2 = 45;
-        // System.out.println(num1/num2);
+        // int num1 = 30;
+        // int num2 = 10;
+        // System.out.println("Integer Division  : " +num1/num2);
 
-        
+        // System.out.println();
+
+        // System.out.println("Without variables : " + 70 / 30);
+
+        // System.out.println();
+
+        // int n1 = 30;
+        // int  n2 = 10;
+        // System.out.println("Floating Division : " +(float) n1/n2);
+
+        // System.out.println();
+
+        // System.out.println("Without Variables : " + 70/30);
+
+        // System.out.println();
+
+        float s1 = 10f;
+        float s2 = 5f;
+        System.out.println("Integer Division : " +(int) s1/(int)s2);
+
+        float s3 = 10;
+        float s4 = 5;
+        System.out.println("Floating Division : " + s3/s4);
+
 
     }
 }
