0dd49f026d4c427f4504933eef91bac4aa5faf7f
Suraj <surajsppokale@gmail.com>
2026-08-27 10:25:03 +0530

    Three Numbers

--- Day7_Activity.java
+++ Day7_Activity.java
@@ -30,7 +30,32 @@ public class Day7_Activity
         // float s4 = 5;
         // System.out.println("Floating Division : " + s3/s4);
 
-        
+        int s1 = 10;
+        int s2 = 20;
+        int s3 = 30;
+
+        System.out.println();
+        System.out.println("Value1 before Swapping : " + s1);
+        System.out.println();
+        System.out.println("Value2 before Swapping :  " + s2);
+        System.out.println();
+        System.out.println("Value3 before Swapping : " + s3);
+
+        s1 = s1 + s2; // Value1 = 30
+        s2 = s3 - s2; // Value2 = 10
+        s3 = s1 - s2; // Value3 = 20 
+
+        System.out.println();
+
+        System.out.println("After Swapping");
+
+        System.out.println();
+        System.out.println("Value1 after Swapping : " + s1);
+        System.out.println();
+        System.out.println("Value2 after Swapping :  " + s2);
+        System.out.println();
+        System.out.println("Value3 after Swapping : " + s3);
+
 
 
     }
