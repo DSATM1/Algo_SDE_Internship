0b021c1b79b0bb7435730bafbeb25532dcda49a1
Suraj <surajsppokale@gmail.com>
2026-08-28 12:38:12 +0530

    Arithmetic Operators

--- Day_9.java
+++ Day_9.java
@@ -2,24 +2,24 @@ import java.util.Arrays;
 
 public class Day_9 
 {
-    static void A()
-    {
-        B();
-    }
+    // static void A()
+    // {
+    //     B();
+    // }
 
-    static void B()
-    {
-        C();
-    }
+    // static void B()
+    // {
+    //     C();
+    // }
 
-    static void C()
-    {
-        System.out.println("Hello");
-    }
+    // static void C()
+    // {
+    //     System.out.println("Hello");
+    // }
     public static void main(String [] args)
     {
-        A();
-        
+        // A();
+
         // int arr[] = new int[5]; // wrong metod
         // System.out.println(arr); // Hashvalue
         // System.out.println(Arrays.toString(arr)); // default value 0 0 0 0 0
@@ -32,6 +32,52 @@ public class Day_9
         // int arr2[] = new int[] {11,5,12};
         // System.out.println(arr2); // Hashvalue
         // System.out.println(Arrays.toString(arr2)); // 10 , 20, 30
+
+        byte a = 10;
+        byte b = 20;
+
+        boolean x = true;
+        boolean y = false;
+
+        // System.out.println(x+y); // in java true is true only not return anything
+
+        System.out.println("A is : " + a);
+        System.out.println("B is : " + b);
+        System.out.println();
+
+        System.out.println("Addition : " + (a+b));
+        System.out.println("Substraction : " + (a-b));
+        System.out.println("Product : " + (a*b));        
+        System.out.println("Division : " + (b/a));
+        System.out.println("Modulos : " + (a%b)); 
+        System.out.println();
+
+
+        System.out.println("Eq 1 : " + (a*(b/2)+3));
+        System.out.println("Eq 2 : " + (10+5*2));
+        System.out.println("Eq 3 : " + ((10+5)*2));
+        System.out.println("Eq 4 : " + (20-5+2));
+        System.out.println("Eq 5 : " + (20-(5+2)));
+        System.out.println();
         
+        --a;
+        System.out.println("--a : " + a);
+        a--;
+        System.out.println("a-- : " + a);
+        ++a;
+        System.out.println("++a : " + a);
+        a++;
+        System.out.println("a++ : " + a);
+        b--;
+        System.out.println("b-- : " + b);
+        --b;
+        System.out.println("--b : " + b);
+        ++b;
+        System.out.println("++b : " + b);
+        b++;
+        System.out.println("b++ : " + b);
+        
+        
+
     }
 }
