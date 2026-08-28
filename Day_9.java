6dbd39311095b7d13921c2e917178a40f2ce604f
Suraj <surajsppokale@gmail.com>
2026-08-28 11:37:39 +0530

    Arrays Methods

--- Day_9.java
+++ Day_9.java
@@ -1,7 +1,24 @@
+import java.util.Arrays;
+
 public class Day_9 
 {
     public static void main(String [] args)
     {
-        System.out.println("Hello");
+        // int arr[] = new int[5]; // wrong metod
+        // System.out.println(arr); // Hashvalue
+        // System.out.println(Arrays.toString(arr)); // default value 0 0 0 0 0
+        // [I@251a69d7 // I = Array Integer, @ = seperator, rest of them is hashvalues 
+        
+        // int arr1[] = {10,20,30};
+        // System.out.println(arr1); // Hashvalue
+        // System.out.println(Arrays.toString(arr1)); // 10 , 20, 30
+
+        // int arr2[] = new int[] {11,5,12};
+        // System.out.println(arr2); // Hashvalue
+        // System.out.println(Arrays.toString(arr2)); // 10 , 20, 30
+
+        
+
+        
     }
 }
