34ea74ca58e14ee0efe8d9e0e87b602ab67a66af
Suraj <surajsppokale@gmail.com>
2026-08-31 12:34:39 +0530

    Inc and Dec

--- Day_10.java
+++ Day_10.java
@@ -5,7 +5,7 @@ public class Day_10
     public static void main(String [] args )
     {
         // Addition of 2 numbers using Scanner Class 
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         //int a,b,c;
         // System.out.print( "Enter the First Number : ");
         // a = sc.nextInt();
@@ -15,15 +15,86 @@ public class Day_10
         // System.out.println( "Addition of " + a + " and " + b + " is : " + c);
         
         // Area of Triangle using Scanner Class 
-        double h,b,tr ;
-        System.out.print( "Enter the Height: ");
-        h = sc.nextDouble();
-        System.out.print( "Enter the Base: ");
-        b = sc.nextDouble();
-        tr = 0.5 * h * b;
-        System.out.print( "Area of Triangle : " + tr);
+        // double h,b,tr ;
+        // System.out.print( "Enter the Height: ");
+        // h = sc.nextDouble();
+        // System.out.print( "Enter the Base: ");
+        // b = sc.nextDouble();
+        // tr = 0.5 * h * b;
+        // System.out.print( "Area of Triangle : " + tr);
+
+        // int a;
+        // int count = 10;
+        // System.out.println(count++);
+        // System.out.println(count);
+        // System.out.println(count++);
+        // System.out.println(count--);
+        // System.out.println(count);
+        // System.out.println(count--);
+
+        // System.out.println();
+
+        // System.out.println(++count);
+        // System.out.println(count);
+        // System.out.println(--count);
+        // System.out.println(--count);
+        // System.out.println(count);
+
+        // int store = count;
+        // store++;
+        // System.out.println(store);
+
+        // int c1 = 12;
+        // int c2 = c1++;
+        // System.out.println(c2);
+        // System.out.println(c1);
+        
+        // int a  = 13;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
 
 
+        // // Behaviour in expression 
 
+        // System.out.println("Behaviour" );
+        // int c = 10;
+        // System.out.println(c++ + 5); // 15
+        // System.out.println(c); // 11
+        // System.out.println(++c + 5); // 17
+        // System.out.println(c); // 12
+
+        // // Confusing Behaviour 
+        // System.out.println("Confusing Behaviour" );
+
+        // int num = 10;
+        // System.out.println(num++ + ++num);
+
+        // int num2 = 11;
+        // System.out.println(num2++ + num2++);
+
+        // int num3 = 11;
+        // System.out.println(--num3 + --num3);
+
+        // int num4 = 11;
+        // System.out.println(--num4 + num4++);
+
+        // System.out.println("Qestions");
+        // int a1 = 10;
+        // int b1 = 5;
+        // System.out.println(a1++ + ++b1);
+        // System.out.println(a1);
+        // System.out.println(b1);
+
+        // System.out.println();
+
+        // int z = 4;
+        // System.out.println(++z + z++ );
+        // System.out.println(z);
+
+        // 10 confusing question 
+        // 2 basic 
+        // 2 medium
+        // 6 difficult
     }
 }
