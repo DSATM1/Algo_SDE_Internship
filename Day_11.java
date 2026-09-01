8f5dac95278318b3faf6e7b04edb9c0de86d7a3c
Suraj <surajsppokale@gmail.com>
2026-09-01 12:19:40 +0530

    updated file

--- Day_11.java
+++ Day_11.java
@@ -5,6 +5,9 @@ public class Day_11
     public static void main(String [] args)
     {
 
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Your Age ");
+        // int age = sc.nextInt();
         // if (age >= 18)
         // {
         //     System.out.println("Eligible to vote");
@@ -29,27 +32,33 @@ public class Day_11
         //     System.out.println("Kid");   
         // }
 
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Your Age ");
+        // int age = sc.nextInt();
+        // System.out.println("Are you Indian Citizenship (True/False) : ");
+        // boolean citi = sc.nextBoolean();
+
+        // if (age >= 18)
+        // {
+        //     if (citi) // Nested if block 
+        //     {
+        //         System.out.println("Eligible");
+        //     }
+        //     else
+        //     {
+        //         System.out.println("Not Eligible");
+        //     }
+        // }
+        // else 
+        // {
+        //     System.out.println("Not Eligible to vote");
+        // }
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your Age ");
         int age = sc.nextInt();
-        System.out.println("Are you Indian Citizenship (True/False) : ");
-        boolean citi = sc.nextBoolean();
 
-        if (age >= 18)
-        {
-            if (citi) // Nested if block 
-            {
-                System.out.println("Eligible");
-            }
-            else
-            {
-                System.out.println("Not Eligible");
-            }
-        }
-        else 
-        {
-            System.out.println("Not Eligible to vote");
-        }
+
         
     }
 }
