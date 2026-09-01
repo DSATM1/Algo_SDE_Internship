bab11a46c4de0eee5b362573c489c68bc42c4fa2
Suraj <surajsppokale@gmail.com>
2026-09-01 13:14:09 +0530

    College Admission

---
+++ Day11_Activity.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Day11_Activity {
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Marks : ");
+        int marks = sc.nextInt();
+        
+        if (marks < 65)
+        {
+            System.out.println("Bye Bye");
+        }
+        else
+        {
+            System.out.println("Passed n all Subjects (True/False) : ");
+            boolean pass = sc.nextBoolean();
+
+            if (marks >= 65 && pass == true)
+            {
+                System.out.println("You are eligible to get a seat");
+            }
+            else
+            {
+                System.out.println("You're not");
+            }
+        }
+        sc.close();
+    }
+}
