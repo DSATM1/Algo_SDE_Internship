3c033fb206d462b37095343df0fec4fc085e59a4
Suraj <surajsppokale@gmail.com>
2026-09-01 11:48:29 +0530

    Voting If else condtions

--- Day_11.java
+++ Day_11.java
@@ -1,7 +1,21 @@
+import java.util.*;
+
 public class Day_11 
 {
     public static void main(String [] args)
     {
-        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Your Age : ");
+        int age = sc.nextInt();
+        if (age >= 18)
+        {
+            System.out.println("Eligible to vote");
+        }
+        else 
+        {
+            System.out.println("Not Eligible");
+        }
+        
     }
 }
