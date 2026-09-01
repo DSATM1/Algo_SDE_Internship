a545adf165a4985fa17c3fab27fdbf3d1372a6bf
Suraj <surajsppokale@gmail.com>
2026-09-01 06:02:08 +0530

    Relational Operator Activity

--- Day_10_Activity.java
+++ Day_10_Activity.java
@@ -3,8 +3,10 @@ public class Day_10_Activity
     public static void main(String [] args) 
     {
 
-        
-        System.out.println("Hello ");
+        byte a = 10;
+        byte b = 5;
+        boolean c = a != b;
+        System.out.println("Hello " + c);
 
     }
 }
