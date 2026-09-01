7baab613d3889a1bcaa05dee71415ec8e1d85a2a
Suraj <surajsppokale@gmail.com>
2026-09-01 11:57:51 +0530

    If else ladder

--- Day_11.java
+++ Day_11.java
@@ -6,16 +6,31 @@ public class Day_11
     {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Your Age : ");
-        int age = sc.nextInt();
-        if (age >= 18)
-        {
-            System.out.println("Eligible to vote");
-        }
-        else 
-        {
-            System.out.println("Not Eligible");
-        }
+        // System.out.println("Enter Your Age ");
+        // int age = sc.nextInt();
+        // if (age >= 18)
+        // {
+        //     System.out.println("Eligible to vote");
+        // }
+        // else 
+        // {
+        //     System.out.println("Not Eligible");
+        // }
+
+        // System.out.print("Enter Your Age : ");
+        // int age = sc.nextInt();
+        // if (age == 18)
+        // {
+        //     System.out.println("Eligible to vote");
+        // }
+        // else if (age > 0 && age < 18)
+        // {
+        //     System.out.println("Not Eligible");
+        // }
+        // else
+        // {
+        //     System.out.println("Kid");   
+        // }
         
     }
 }
