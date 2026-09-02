2748a26badb1a423893a5b4380928dfeae6a96ff
Suraj <surajsppokale@gmail.com>
2026-09-02 11:15:46 +0530

    String methods .equals

--- Day_12.java
+++ Day_12.java
@@ -4,13 +4,18 @@ public class Day_12
     {
         int a = 10;
         int b = 20;
-        if (a == b)
+
+        // if (a == b) // works for primitive datatype
+        // if(a.equals(b)) // won't work for primitive datatype // object invoking 
+        
+        String day="Saturday"; // class -> objects > methods > string methods .equals() is a methods 
+        if (day.equals(day)) // works for non primitive data type
         {
-            System.out.println("Equals");    
+            System.out.println("Holiday");    
         }
         else
         {
-            System.out.println("Not Equals");
+            System.out.println("Working Day");
         }
         System.out.println();
     }
