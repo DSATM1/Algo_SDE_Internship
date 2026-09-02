e2bfcb1bd8f42dd7c79f063afea4d6c382e16011
Suraj <surajsppokale@gmail.com>
2026-09-02 11:09:59 +0530

    Day 12 == Operators

---
+++ Day_12.java
@@ -0,0 +1,17 @@
+public class Day_12 
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        if (a == b)
+        {
+            System.out.println("Equals");    
+        }
+        else
+        {
+            System.out.println("Not Equals");
+        }
+        System.out.println();
+    }
+}
