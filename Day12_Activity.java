d64338b1762fd2797a76f3a2e4cd70fb67248578
Suraj <surajsppokale@gmail.com>
2026-09-03 11:17:50 +0530

    Number System

--- Day12_Activity.java
+++ Day12_Activity.java
@@ -1,7 +1,24 @@
+import java.util.*;
+
 public class Day12_Activity 
 {
     public static void main(String [] args)
     {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("1. Binary.");
+        System.out.println("2. Octal");
+        System.out.println("3. Hexadecimal");
+        System.out.print("Enter your choice : ");
+        int choice = sc.nextInt();
+        System.out.print("Enter the Number : ");
+        int num = sc.nextInt();
+
+        if (choice == 1)
+        {
+
+        }
+
         System.out.println("Hello");
+        sc.close();
     }
 }
