1c827b3c82cd469ac0d2564889ae8acfb8ecca53
Suraj <surajsppokale@gmail.com>
2026-09-03 22:25:33 +0530

    Inc by 3 steps for  loop in Day 13 Activity

---
+++ Day13_Activity.java
@@ -0,0 +1,10 @@
+public class Day13_Activity {
+    public static void main(String [] args)
+    {
+        // // Inc by 3 steps 
+        // for (int index = 1; index <=20; index+=3)
+        // {
+        //     System.out.println(index);
+        // }
+    }
+}
