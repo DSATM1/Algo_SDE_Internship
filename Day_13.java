943512ba45950171ea0940c034ea769999cd0425
Suraj <surajsppokale@gmail.com>
2026-09-03 12:16:49 +0530

    For loop

--- Day_13.java
+++ Day_13.java
@@ -1,26 +1,59 @@
 public class Day_13 {
     public static void main(String [] args)
     {
-        System.out.println("Scenario 1 : Both Strings Literals");
-        String s1 = "Java";
-        String s2 = "Java";
+        // System.out.println("Scenario 1 : Both Strings Literals");
+        // String s1 = "Java";
+        // String s2 = "Java";
 
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
+        // System.out.println(s1 == s2);
+        // System.out.println(s1.equals(s2));
         
-        System.out.println("Scenario 2 : Both using New ");
-        String s3 = new String("Java");
-        String s4 = new String("Java");
+        // System.out.println("Scenario 2 : Both using New ");
+        // String s3 = new String("Java");
+        // String s4 = new String("Java");
 
-        System.out.println(s3 == s4);
-        System.out.println(s3.equals(s4));
+        // System.out.println(s3 == s4);
+        // System.out.println(s3.equals(s4));
 
-        System.out.println("Scenario 3 : Literals vs New ");
-        String s5 = "Java";
-        String s6 = new String("Java");
+        // System.out.println("Scenario 3 : Literals vs New ");
+        // String s5 = "Java";
+        // String s6 = new String("Java");
 
-        System.out.println(s5 == s6);
-        System.out.println(s5.equals(s6));
+        // System.out.println(s5 == s6);
+        // System.out.println(s5.equals(s6));
+
+        // for (int i = 1; i <= 5; i++)
+        // {
+        //     // 1 step forward 
+        //     System.out.println(i + " Hello");
+
+        // }
+
+        // for (int i = 1; i <= 5;)
+        // {
+        //     // Infinite loop 
+        //     System.out.println(i + " Hello");
+
+        // }
+
+        // for (int i = 0; i <= 10; i+=2)
+        // { 
+        //     // even 
+        //     System.out.println(i);
+        // }
+
+        // for ( ; ;)
+        //     {
+        //         System.out.println();
+        //     } 
+
+        // for (int i = 1; i <= 10; i+=2)
+        // { 
+        //     // odd 
+        //     System.out.println(i);
+        // }
+
+
 
     }
 }
