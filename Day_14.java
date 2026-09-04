28a30a3638f6d1f476407458faf884e0d8da725e
Suraj <surajsppokale@gmail.com>
2026-09-04 12:10:06 +0530

    multiplication table using while and do- while in Day_14.java

--- Day_14.java
+++ Day_14.java
@@ -5,13 +5,29 @@ public class Day_14
     public static void main(String [] args)
     {
         Scanner sc = new Scanner(System.in);
-        // System.out.print("Enter the Number for Multiplication Table");
-        // int multi = sc.nextInt();
+        System.out.print("Enter the Number for Multiplication Table");
+        int multi = sc.nextInt();
 
         // for (int index= 1;index<=10;index++)
         // {
         //     System.out.println(multi + " X " + index + " = " + (multi*index));
         // }
+
+        // int i = 1;
+        // while(i<=10)
+        // {
+        //     System.out.println(multi + " X " + i + " = " + (multi*i));
+        //     i++;
+        // }
+
+        // int i =1;
+        // do
+        // {
+        //     System.out.println(multi + " X " + i + " = " + (multi*i));
+        //     i++;
+        // }while(i<=10);
+
+
         
         // System.out.print("Enter the Start number : ");
         // int start = sc.nextInt();
