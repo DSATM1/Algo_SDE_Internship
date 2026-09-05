c5f8825ebd966042f52a095b9492107ab3f2406f
Suraj <surajsppokale@gmail.com>
2026-09-05 12:35:09 +0530

    Comment out unused code for octal validation

--- Day_14.java
+++ Day_14.java
@@ -4,31 +4,31 @@ public class Day_14
 {
     public static void main(String [] args)
     {
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the Number");
-        String num=sc.next(); // "127" --> word
-        boolean octal = true; // assume that the number is octal 
+        // System.out.print("Enter the Number");
+        // String num=sc.next(); // "127" --> word
+        // boolean octal = true; // assume that the number is octal 
 
-        for(int i = 0; i <= num.length() - 1; i++)
-        {
-            char ch = num.charAt(i);
+        // for(int i = 0; i <= num.length() - 1; i++)
+        // {
+        //     char ch = num.charAt(i);
 
-            if (ch < '0' || ch > '7')
-            {
-                octal = false;
-                break;
-            }
+        //     if (ch < '0' || ch > '7')
+        //     {
+        //         octal = false;
+        //         break;
+        //     }
             
-        }
-            if(octal)
-            {
-                System.out.println("Yes radix = 8");
-            }
-            else
-            {
-                System.out.println("Not an octal");
-            }
+        // }
+        //     if(octal)
+        //     {
+        //         System.out.println("Yes radix = 8");
+        //     }
+        //     else
+        //     {
+        //         System.out.println("Not an octal");
+        //     }
         
 
         // System.out.print("Enter the Number for Multiplication Table");
@@ -92,6 +92,6 @@ public class Day_14
         //     System.out.println(i/=2);
         // }
 
-        sc.close();
+        //sc.close();
     }
 }
