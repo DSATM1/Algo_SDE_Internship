012e410e53935d34ec50913c8481727470ebb428
Suraj <surajsppokale@gmail.com>
2026-09-06 12:57:24 +0530

    Factorial code in Day14_Activity

--- Day14_Activity.java
+++ Day14_Activity.java
@@ -1,105 +1,127 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Day14_Activity {
-    public static void main(String [] args)
-    {
-        // Scanner sc = new Scanner(System.in);
+// public class Day14_Activity {
+//     public static void main(String [] args)
+//     {
+//         // Scanner sc = new Scanner(System.in);
 
-        // System.out.print("Enter the Number");
-        // String num=sc.next(); // "127" --> word
-        // boolean octal = true; // assume that the number is octal 
+//         // System.out.print("Enter the Number");
+//         // String num=sc.next(); // "127" --> word
+//         // boolean octal = true; // assume that the number is octal 
 
-        // for(int i = 0; i <= num.length() - 1; i++)
-        // {
-        //     char ch = num.charAt(i);
+//         // for(int i = 0; i <= num.length() - 1; i++)
+//         // {
+//         //     char ch = num.charAt(i);
 
-        //     if (ch < '0' || ch > '7')
-        //     {
-        //         octal = false;
-        //         break;
-        //     }
+//         //     if (ch < '0' || ch > '7')
+//         //     {
+//         //         octal = false;
+//         //         break;
+//         //     }
             
-        // }
-        // if(octal)
-        // {
-        //     System.out.println("Yes radix = 8");
-        // }
-        // else
-        // {
-        //     System.out.println("Not an octal");
-        // }
-        // sc.close();
+//         // }
+//         // if(octal)
+//         // {
+//         //     System.out.println("Yes radix = 8");
+//         // }
+//         // else
+//         // {
+//         //     System.out.println("Not an octal");
+//         // }
+//         // sc.close();
 
-        // ------>>> Dec by half i/2 <<<<-------
+//         // ------>>> Dec by half i/2 <<<<-------
 
-        // for (int i = 128; i >=1 ; )
-        // {
-        //     System.out.print( " " + (i/=2));
-        // }
+//         // for (int i = 128; i >=1 ; )
+//         // {
+//         //     System.out.print( " " + (i/=2));
+//         // }
 
-        // int i = 128;
-        // while (i>=1)
-        // {
-        //     System.out.print(" " + (i/=2));
-        // }
+//         // int i = 128;
+//         // while (i>=1)
+//         // {
+//         //     System.out.print(" " + (i/=2));
+//         // }
 
-        // int i = 128;
-        // do 
-        // {
-        //     System.out.print(" " + (i/=2));
-        // } while(i>=1);
-
-
-        // ------>>>>> 100,90,80,70,,,,10 <<<<<--------
-
-        // for (int i = 100; i>=10; i-=10)
-        // {
-        //     System.out.print(" " + i);
-        // }
-
-        // int i = 100;
-        // while (i>=10)
-        // {
-        //     System.out.print(" " + i);
-        //     i-=10;
-        // }
-
-        // int i = 100;
-        // do
-        // {
-        //     System.out.print(" " + i);
-        //     i-=10;
-        // }while(i>=10);
+//         // int i = 128;
+//         // do 
+//         // {
+//         //     System.out.print(" " + (i/=2));
+//         // } while(i>=1);
 
 
-        // ---->>> multiple of 10 <<<<-----
-        // for (int i = 1; i<=1000; i*=10)
-        // {
-        //     System.out.print(" " + i);
-        // }
+//         // ------>>>>> 100,90,80,70,,,,10 <<<<<--------
 
-        // int i = 1;
-        // while (i<=1000)
-        // {
-        //     System.out.print(" " + i);
-        //     i*=10;
-        // }
+//         // for (int i = 100; i>=10; i-=10)
+//         // {
+//         //     System.out.print(" " + i);
+//         // }
 
-        // int i = 1;
-        // do
-        // {
-        //     System.out.print(" " + i);
-        //     i*=10;
-        // }while(i<=1000);
+//         // int i = 100;
+//         // while (i>=10)
+//         // {
+//         //     System.out.print(" " + i);
+//         //     i-=10;
+//         // }
+
+//         // int i = 100;
+//         // do
+//         // {
+//         //     System.out.print(" " + i);
+//         //     i-=10;
+//         // }while(i>=10);
 
 
-        // int fact = 5;
-        // int res = 1 ;
-        // for (int i = 1; i<=fact; i++)
-        // {
-        //     res = res*i;    
+//         // ---->>> multiple of 10 <<<<-----
+//         // for (int i = 1; i<=1000; i*=10)
+//         // {
+//         //     System.out.print(" " + i);
+//         // }
 
-        // }
-        // System.out.println(res);
-    }
-}
+//         // int i = 1;
+//         // while (i<=1000)
+//         // {
+//         //     System.out.print(" " + i);
+//         //     i*=10;
+//         // }
+
+//         // int i = 1;
+//         // do
+//         // {
+//         //     System.out.print(" " + i);
+//         //     i*=10;
+//         // }while(i<=1000);
+
+//         // Factorial of number 
+//         // int fact = 5;
+//         // int res = 1 ;
+//         // for (int i = 1; i<=fact; i++)
+//         // {
+//         //     res = res*i;    
+
+//         // }
+//         // System.out.println(res);
+
+//         // Scanner sc = new Scanner(System.in);
+//         // System.out.print("Enter the Number : ");
+//         // int fact = sc.nextInt();
+//         // int res = 1;
+//         // int i = 1;
+
+//         // while(i<=fact)
+//         // {
+//         //     res=res*i;
+//         //     i++;
+//         // }
+//         // System.out.println(" " + res);
+
+//         // do
+//         // {
+//         //     res*=i;
+//         //     i++;
+//         // }while(i<=fact);
+//         // System.out.println("Factorial of " + fact + " is : " + res);
+
+//         // sc.close();
+//     }
+// }
