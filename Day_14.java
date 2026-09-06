90099eae66d468965d006755eb71a896f97d29af
Suraj <surajsppokale@gmail.com>
2026-09-06 12:56:37 +0530

    Comment out unused code in Day_14.java for clarity

--- Day_14.java
+++ Day_14.java
@@ -1,96 +1,96 @@
-import java.util.*;
+// import java.util.*;
 
-public class Day_14 
-{
-    public static void main(String [] args)
-    {
-        // Scanner sc = new Scanner(System.in);
+// public class Day_14 
+// {
+//     public static void main(String [] args)
+//     {
+//         // Scanner sc = new Scanner(System.in);
 
-        // System.out.print("Enter the Number");
-        // String num=sc.next(); // "127" --> word
-        // boolean octal = true; // assume that the number is octal 
+//         // System.out.print("Enter the Number");
+//         // String num=sc.next(); // "127" --> word
+//         // boolean octal = true; // assume that the number is octal 
 
-        // for(int i = 0; i <= num.length() - 1; i++)
-        // {
-        //     char ch = num.charAt(i);
+//         // for(int i = 0; i <= num.length() - 1; i++)
+//         // {
+//         //     char ch = num.charAt(i);
 
-        //     if (ch < '0' || ch > '7')
-        //     {
-        //         octal = false;
-        //         break;
-        //     }
-        // }
-        //     if(octal)
-        //     {
-        //         System.out.println("Yes radix = 8");
-        //     }
-        //     else
-        //     {
-        //         System.out.println("Not an octal");
-        //     }
+//         //     if (ch < '0' || ch > '7')
+//         //     {
+//         //         octal = false;
+//         //         break;
+//         //     }
+//         // }
+//         //     if(octal)
+//         //     {
+//         //         System.out.println("Yes radix = 8");
+//         //     }
+//         //     else
+//         //     {
+//         //         System.out.println("Not an octal");
+//         //     }
         
-        // ------------- >>>>>> Multiplication Table <<<<<<< -----------
+//         // ------------- >>>>>> Multiplication Table <<<<<<< -----------
 
-        // System.out.print("Enter the Number for Multiplication Table");
-        // int multi = sc.nextInt();
+//         // System.out.print("Enter the Number for Multiplication Table");
+//         // int multi = sc.nextInt();
 
-        // for (int index= 1;index<=10;index++)
-        // {
-        //     System.out.println(multi + " X " + index + " = " + (multi*index));
-        // }
+//         // for (int index= 1;index<=10;index++)
+//         // {
+//         //     System.out.println(multi + " X " + index + " = " + (multi*index));
+//         // }
 
-        // int i = 1;
-        // while(i<=10)
-        // {
-        //     System.out.println(multi + " X " + i + " = " + (multi*i));
-        //     i++;
-        // }
+//         // int i = 1;
+//         // while(i<=10)
+//         // {
+//         //     System.out.println(multi + " X " + i + " = " + (multi*i));
+//         //     i++;
+//         // }
         
-        // int i =1;
-        // do
-        // {
-        //     System.out.println(multi + " X " + i + " = " + (multi*i));
-        //     i++;
-        // }while(i<=10);
+//         // int i =1;
+//         // do
+//         // {
+//         //     System.out.println(multi + " X " + i + " = " + (multi*i));
+//         //     i++;
+//         // }while(i<=10);
 
-        //------- >>>>>> Power of 2 <<<<<< --------------
+//         //------- >>>>>> Power of 2 <<<<<< --------------
 
-        // System.out.print("Enter the Start number : ");
-        // int start = sc.nextInt();
+//         // System.out.print("Enter the Start number : ");
+//         // int start = sc.nextInt();
 
-        // System.out.print("Enter the End number : ");
-        // int end = sc.nextInt();
+//         // System.out.print("Enter the End number : ");
+//         // int end = sc.nextInt();
 
-        // for (int i = start; i<= end; i++)
-        // {
-        //     System.out.print(" " + i*i);
-        // }
+//         // for (int i = start; i<= end; i++)
+//         // {
+//         //     System.out.print(" " + i*i);
+//         // }
 
-        // int i = start;
-        // while(i<=end)
-        //     {
-        //         System.out.print(" " + i*i);
-        //         i++;
-        //     } 
+//         // int i = start;
+//         // while(i<=end)
+//         //     {
+//         //         System.out.print(" " + i*i);
+//         //         i++;
+//         //     } 
 
-        // do
-        // {
-        //     System.out.print(" " + i*i);
-        //     i++;
-        // }while(i<=end);
+//         // do
+//         // {
+//         //     System.out.print(" " + i*i);
+//         //     i++;
+//         // }while(i<=end);
 
-        // System.out.print("Enter the End number : ");
-        // int end = sc.nextInt();
+//         // System.out.print("Enter the End number : ");
+//         // int end = sc.nextInt();
         
-        // System.out.print("Enter the Start number : ");
-        // int start = sc.nextInt();
+//         // System.out.print("Enter the Start number : ");
+//         // int start = sc.nextInt();
 
 
-        // for (int i = 64; i >=1 ; )
-        // {
-        //     System.out.println(i/=2);
-        // }
+//         // for (int i = 64; i >=1 ; )
+//         // {
+//         //     System.out.println(i/=2);
+//         // }
 
-        //sc.close();
-    }
-}
+//         //sc.close();
+//     }
+// }
