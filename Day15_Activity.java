adcc3461e4fa59b5889bea4ac30d8ef166d43080
Suraj <surajsppokale@gmail.com>
2026-09-07 22:43:00 +0530

    Add nested loop example for clarity in Day15_Activity.java

--- Day15_Activity.java
+++ Day15_Activity.java
@@ -93,6 +93,15 @@ public class Day15_Activity {
         //     }
         //     System.out.println(i);
         // }
+
+        // for (int i = 1; i<=3;i++)
+        // {
+        //     for (int j = 1; j<= 3; j++)
+        //     {
+        //         System.out.print(" " + "*" + i + "A");
+        //     }
+        //     System.out.println(" " );
+        // }
         sc.close();
     }
 }
