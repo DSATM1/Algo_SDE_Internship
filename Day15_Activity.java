9b90c1c7289668deef67990f72900bd0826866c5
Suraj <surajsppokale@gmail.com>
2026-09-08 22:40:41 +0530

    Refactor Day15_Activity.java to comment out unused code for clarity

--- Day15_Activity.java
+++ Day15_Activity.java
@@ -1,107 +1,140 @@
-import java.util.*;
+// import java.util.*;
 
-public class Day15_Activity {
-    public static void main(String [] args)
-    {
+// public class Day15_Activity {
+//     public static void main(String [] args)
+//     {
 
-        Scanner sc = new Scanner(System.in);
-        // System.out.print("Enter the number : ");
-        // int num = sc.nextInt();
+//         // For Using in String num extration and number printing;
+//         // Scanner sc = new Scanner(System.in);
+//         // System.out.print("Enter the number (It will tae it as String) : ");
+//         // String num = sc.next();
 
-        // // Reverse a number 
-        // long rev = 0;
-        // while(n > 0)
-        // {
-        //     long digit = n % 10;
-        //     n = n / 10;
-        //     rev = rev * 10 + digit;
-        // }
-        // System.out.println(rev);
+//         // Integer.parseInt(num);
+//         // System.out.print("Enter the number (It will tae it as Integer) : ");
+//         // int n = sc.nextInt();
 
-        // using for loop ---->>>>>>
-        // int rev = 0;
-        // for (int i = rev; )
-        // {
-        //     int digit = n % 10;
-        //     n = n/ 10;
-        //     rev = rev * 10 + digit;
-        // }
-        // System.out.println(rev);
+//         // for (int i = 0; i < n;)
+//         // {
+//         //     int d = n % 10;
+//         //     n = n /10;
+//         //     System.out.println(d);
+//         // }
 
-        // palindrome 
-        // int temp = num;
-        // int rev = 0;
-        // while (num > 0 )
-        // {
-        //     int digit = num % 10;
-        //     rev = rev * 10 + digit;
-        //     num = num / 10; //0 == 121 ---> fail 
-        // }
-        // if (temp == rev)
-        // {
-        //     System.out.println("Is Palindrome");
-        // }
-        // else
-        // {
-        //     System.out.println("Is Not Palindrome");
-        // }
+//         // while(n > 0)
+//         // {   
+//         //     int d = n % 10;
+//         //     n = n/10;
+//         //     System.out.println(d);
+//         // }
+//         // int temp = num;
+//         // int result  = 0;
+//         // int rev = 0;
+//         // int amst = 0;
 
-        // Break at 5
-        // for (int i= 1; i<=10; i++)
-        // {
-        //     if(i == 5)
-        //         break;
-        //     System.out.println(i);
-        // }
+//         // while(num > 0)
+//         // {
+//         //     num = num % 10;
+//         //     rev = rev ;
+//         //     num = num / 10;
+//         // }
+//         // if ( temp == rev)
 
-        // Alphabet Break
-        // for (char i = 'A'; i<= 'Z'; i++)
-        // {
-        //     if (i == 'M')
-        //     {
-        //         break;
-        //     }
-        //         System.out.print(" " + i);
-        // }
 
-        // // Until get 7
-        // for (int i = 1; i<=10; i++)
-        // {
-        //     if (i == 7)
-        //     {
-        //         break;
-        //     }
-        //     System.out.print(" " +i);
-        // }
 
-        // Even Numbers 
-        // for (int i = 0; i<= 20;i++)
-        // {
-        //     if ( i % 2 != 0)
-        //     {
-        //         continue;
-        //     }
-        //     System.out.println(i);
-        // }
+//         // // Reverse a number 
+//         // long rev = 0;
+//         // while(n > 0)
+//         // {
+//         //     long digit = n % 10;
+//         //     n = n / 10;
+//         //     rev = rev * 10 + digit;
+//         // }
+//         // System.out.println(rev);
 
-        // Odd Numbers
-        // for (int i = 0; i<= 20;i++)
-        // {
-        //     if ( i % 2 == 0)
-        //     {
-        //         continue;
-        //     }
-        //     System.out.println(i);
-        // }
+//         // using for loop ---->>>>>>
+//         // int rev = 0;
+//         // for (int i = rev; )
+//         // {
+//         //     int digit = n % 10;
+//         //     n = n/ 10;
+//         //     rev = rev * 10 + digit;
+//         // }
+//         // System.out.println(rev);
 
-        // for (int i = 1; i<=3;i++)
-        // {
-        //     for (int j = 1; j<= 3; j++)
-        //     {
-        //         System.out.print(" " + "*" + i + "A");
-        //     }
-        //     System.out.println(" " );
-        // }
-        sc.close();
-    }
-}
+//         // palindrome 
+//         // int temp = num;
+//         // int rev = 0;
+//         // while (num > 0 )
+//         // {
+//         //     int digit = num % 10;
+//         //     rev = rev * 10 + digit;
+//         //     num = num / 10; //0 == 121 ---> fail 
+//         // }
+//         // if (temp == rev)
+//         // {
+//         //     System.out.println("Is Palindrome");
+//         // }
+//         // else
+//         // {
+//         //     System.out.println("Is Not Palindrome");
+//         // }
+
+//         // Break at 5
+//         // for (int i= 1; i<=10; i++)
+//         // {
+//         //     if(i == 5)
+//         //         break;
+//         //     System.out.println(i);
+//         // }
+
+//         // Alphabet Break
+//         // for (char i = 'A'; i<= 'Z'; i++)
+//         // {
+//         //     if (i == 'M')
+//         //     {
+//         //         break;
+//         //     }
+//         //         System.out.print(" " + i);
+//         // }
+
+//         // // Until get 7
+//         // for (int i = 1; i<=10; i++)
+//         // {
+//         //     if (i == 7)
+//         //     {
+//         //         break;
+//         //     }
+//         //     System.out.print(" " +i);
+//         // }
+
+//         // Even Numbers 
+//         // for (int i = 0; i<= 20;i++)
+//         // {
+//         //     if ( i % 2 != 0)
+//         //     {
+//         //         continue;
+//         //     }
+//         //     System.out.println(i);
+//         // }
+
+//         // Odd Numbers
+//         // for (int i = 0; i<= 20;i++)
+//         // {
+//         //     if ( i % 2 == 0)
+//         //     {
+//         //         continue;
+//         //     }
+//         //     System.out.println(i);
+//         // }
+
+//         // for (int i = 1; i<=3;i++)
+//         // {
+//         //     for (int j = 1; j<= 3; j++)
+//         //     {
+//         //         System.out.print(" " + "*" + i + "A");
+//         //     }
+//         //     System.out.println(" " );
+//         // }
+//         // sc.close();
+//     }
+// }
