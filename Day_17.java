efc87328a889578e423c5e8bd7912d914c3168ce
Suraj <surajsppokale@gmail.com>
2026-09-09 11:44:07 +0530

    Refactor Day_17.java to implement max function and comment out unused Name method

--- Day_17.java
+++ Day_17.java
@@ -1,11 +1,25 @@
 public class Day_17 
 {
-    static void Name()
+    static int max(int x,int  y) // x = 10, y = 15
     {
-        System.out.println("Hello Suraj S P");
+        if (x > y)
+        {
+            return x;
+        }
+        else
+        {
+            return y; //return 15
+        }
     }
+    // static void Name()
+    // {
+    //     System.out.println("Hello Suraj S P");
+    // }
     public static void main(String [] args)
     {
-        Name();
+        int a = 10, b = 15;
+        //Name();
+        int c = max(a, b);
+        System.out.println(c);
     }
 }
