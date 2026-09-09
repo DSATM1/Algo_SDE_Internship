65b064274e900d52e0797937994f3bbc27ecc5e2
Suraj <surajsppokale@gmail.com>
2026-09-09 21:48:14 +0530

    Methods - static add method

--- surajObject.java
+++ surajObject.java
@@ -1,11 +1,16 @@
+// import java.util.*;
+
 public class surajObject
+
 //class Suraj 
 {
+    // Greetings 
     // static void Hello()
     // {
     //     System.out.println("Good Eve Suraj S P");
     // }
 
+    // Smaller Number 
     // static int New_Num(int x , int y)
     // {
     //     if (x < y)
@@ -17,15 +22,71 @@ public class surajObject
     //         return y;
     //     }
     // }
+
+    // Addition
+    static void add(int x, int y)
+    {
+        System.out.println("Addition of " + x + " and " + y + " is " + (x+y));
+    }
+
+
     public static void main(String [] args)
     {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter 1st Number : ");
+        // int x = sc.nextInt();
+        // System.out.print("Enter 2nd Number : ");
+        // int y = sc.nextInt();
+
         // surajObject sp = new surajObject();
+
+        // int a = sp.add(8, 9);
+        // System.out.println(a);
+
+        add(10,20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // sp.Hello();
         // int n = sp.New_Num(10, 1);
         // System.out.println(n);
 
-        //Hello();
-        //int n = New_Num(5, 10);
+        //Hello(); // 1
+
+        //int n = New_Num(5, 10); // 2
         //System.out.println(n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //sc.close();
     }
 }
