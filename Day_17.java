4712e7fdc54bde682496b421d892ba95b283674c
Suraj <surajsppokale@gmail.com>
2026-09-09 11:28:49 +0530

    Static Methods in java

---
+++ Day_17.java
@@ -0,0 +1,11 @@
+public class Day_17 
+{
+    static void Name()
+    {
+        System.out.println("Hello Suraj S P");
+    }
+    public static void main(String [] args)
+    {
+        Name();
+    }
+}
