e66e3de7a9fd377d2b847edc585562d80139a3b7
Suraj <surajsppokale@gmail.com>
2026-09-09 12:11:15 +0530

    Organize static methods for clarity

--- Day_17.java
+++ Day_17.java
@@ -18,6 +18,39 @@ public class Day_17
     // {
     //     System.out.println("Hello Suraj S P");
     // }
+
+
+    // No args No Return 
+    static void greet()
+    {
+        System.out.println("Hiiii"); 
+    }
+
+    // Args but No return  
+    static void Sum(int a, int b)
+    {
+        System.out.println("Sum = " + (a+b));
+    }
+
+    // No args But return 
+    static int getNum()
+    {
+        return 100;
+    }
+
+    // Args and Return 
+    static int max(int x,int  y) 
+    {
+        if (x > y)
+        {
+            return x;
+        }
+        else
+        {
+            return y; //return 15
+        }
+    }
+
     public static void main(String [] args)
     {
         // int a = 10, b = 15; // parameter passing -- send data // Actual Parameters 
@@ -25,5 +58,19 @@ public class Day_17
         // int c = max(a, b);
         // System.out.println(c);
         //System.out.println(x) // a is not in this method that's why it showing Error
+
+        // 1
+        greet();
+
+        // 2
+        Sum(10, 20);
+
+        // 3
+        int num = getNum();
+        System.out.println(num);
+
+        // 4
+        int larg = max(25, 40); // to catch the return value 
+        System.out.println(larg);
     }
 }
