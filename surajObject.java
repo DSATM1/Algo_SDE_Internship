cf5e1911edfd294a872673e2f5e8f840777a80b0
Suraj <surajsppokale@gmail.com>
2026-09-09 19:20:32 +0530

    Add surajObject.java with example methods and main function

---
+++ surajObject.java
@@ -0,0 +1,31 @@
+public class surajObject
+//class Suraj 
+{
+    static void Hello()
+    {
+        System.out.println("Good Eve Suraj S P");
+    }
+
+    static int New_Num(int x , int y)
+    {
+        if (x < y)
+        {
+            return x;
+        }
+        else
+        {
+            return y;
+        }
+    }
+    public static void main(String [] args)
+    {
+        // surajObject sp = new surajObject();
+        // sp.Hello();
+        // int n = sp.New_Num(10, 1);
+        // System.out.println(n);
+
+        //Hello();
+        //int n = New_Num(5, 10);
+        //System.out.println(n);
+    }
+}
