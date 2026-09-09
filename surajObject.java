24f729802c3b6465f99dd5d784ac1ccf1e95bcde
Suraj <surajsppokale@gmail.com>
2026-09-09 21:59:58 +0530

    Method - Square using Static

--- surajObject.java
+++ surajObject.java
@@ -24,24 +24,26 @@ public class surajObject
     // }
 
     // Addition
-    int add(int x, int y)
+    // int add(int x, int y)
+    // {
+    //     return (x+y);
+    // }
+
+    // Square of Numbers 
+    static int Square(int a)
     {
-        return (x+y);
+        return a*a;
     }
 
 
     public static void main(String [] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter 1st Number : ");
-        int x = sc.nextInt();
-        System.out.print("Enter 2nd Number : ");
-        int y = sc.nextInt();
-
-        surajObject sp = new surajObject();
-
-        int a = sp.add(x, y);
-        System.out.println("Addition of " + x + " and " + y + " is " + a);
+        System.out.print("Enter the Number for Square: ");
+        int a = sc.nextInt();
+        
+        int s = Square(a);
+        System.out.print("Square of " + a + " is " + s);
 
 
 
@@ -65,27 +67,38 @@ public class surajObject
 
 
 
-        // sp.Hello();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // System.out.print("Enter 1st Number : "); // --------------33333
+        // int x = sc.nextInt();
+        // System.out.print("Enter 2nd Number : ");
+        // int y = sc.nextInt();
+
+        // surajObject sp = new surajObject();
+
+        // int a = sp.add(x, y);
+        // System.out.println("Addition of " + x + " and " + y + " is " + a);
+
+        // sp.Hello(); ------- 1111oooooo
         // int n = sp.New_Num(10, 1);
         // System.out.println(n);
 
-        //Hello(); // 1
+        //Hello(); // -------------111111
 
-        //int n = New_Num(5, 10); // 2
+        //int n = New_Num(5, 10); // -------------22222
         //System.out.println(n);
-
-
-
-
-
-
-
-
-
-
-
-
-
         sc.close();
     }
 }
