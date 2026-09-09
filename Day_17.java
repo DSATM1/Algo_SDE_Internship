d68e0b4bf7fb6957734f644c18a1077737a22e24
Suraj <surajsppokale@gmail.com>
2026-09-09 11:58:45 +0530

    Comment out max function and related code in Day_17.java for clarity

--- Day_17.java
+++ Day_17.java
@@ -1,25 +1,29 @@
 public class Day_17 
 {
-    static int max(int x,int  y) // x = 10, y = 15
-    {
-        if (x > y)
-        {
-            return x;
-        }
-        else
-        {
-            return y; //return 15
-        }
-    }
+    // static int max(int x,int  y) // x = 10, y = 15 // receive data - Formal Parameters 
+    // {
+    //     if (x > y)
+    //     {
+    //         return x;
+    //     }
+    //     else
+    //     {
+    //         return y; //return 15
+    //     }
+
+    //     // Scope Checking 
+    //     // System.out.println(a) // a is not in this method that's why it showing error
+    // }
     // static void Name()
     // {
     //     System.out.println("Hello Suraj S P");
     // }
     public static void main(String [] args)
     {
-        int a = 10, b = 15;
-        //Name();
-        int c = max(a, b);
-        System.out.println(c);
+        // int a = 10, b = 15; // parameter passing -- send data // Actual Parameters 
+        // //Name();
+        // int c = max(a, b);
+        // System.out.println(c);
+        //System.out.println(x) // a is not in this method that's why it showing Error
     }
 }
