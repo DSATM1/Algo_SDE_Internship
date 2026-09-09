ef69b808bcfa034d16fe6d023f7f99a330c680ad
Suraj <surajsppokale@gmail.com>
2026-09-09 13:38:11 +0530

    Add ObjectForEach.java with example methods and main function

---
+++ ObjectForEach.java
@@ -0,0 +1,60 @@
+// public class ObjectForEach 
+// {
+//     // No args No Return 
+//     void greet()
+//     {
+//         System.out.println("Hiiii"); 
+//     }
+
+//     // Args but No return  
+//     void Sum(int a, int b)
+//     {
+//         System.out.println("Sum = " + (a+b));
+//     }
+
+//     // No args But return 
+//     int getNum()
+//     {
+//         return 100;
+//     }
+
+//     // Args and Return 
+//     int max(int x,int  y) 
+//     {
+//         if (x > y)
+//         {
+//             return x;
+//         }
+//         else
+//         {
+//             return y; //return 15
+//         }
+//     }
+// public static void main(String [] args)
+//     {
+//         ObjectForEach sp = new ObjectForEach();
+//         sp.greet();
+//         sp.Sum(10,20);
+//         int s1 = sp.getNum();
+//         System.out.println(s1);
+//         int s2 = sp.max(30,50);
+//         System.out.println(s2);
+
+//         // ObjectForEach obj1 = new ObjectForEach();
+//         // // 1
+//         // obj1.greet();
+
+//         // // 2
+//         // ObjectForEach obj2 = new ObjectForEach();
+//         // obj2.Sum(10,20);
+
+//         // ObjectForEach obj3 = new ObjectForEach();
+//         // int n = obj3.getNum();
+//         // System.out.println(n);
+
+//         // ObjectForEach obj4 = new ObjectForEach();
+//         // int large = obj4.max(2,4);
+//         // System.out.println(large);
+
+//     }
+// }
