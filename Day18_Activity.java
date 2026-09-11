f498413604ddd6d94f86f00447ad6f5687bd7e96
Suraj <surajsppokale@gmail.com>
2026-09-11 09:56:56 +0530

    Add Day18_Activity.java to print odd numbers up to 5

---
+++ Day18_Activity.java
@@ -0,0 +1,12 @@
+public class Day18_Activity 
+{
+
+    public static void main(String[] a)
+    {
+        int n = 5;
+        for (int i = 1;i <= n;i+=2)
+        {
+            System.out.println(i);
+        }
+    }    
+}
