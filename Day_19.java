86a82bd30f9d8c74db5480cee32933eb71a38d19
Suraj <surajsppokale@gmail.com>
2026-09-11 12:05:25 +0530

    Refactor Day_19.java to clean up commented-out methods and improve readability

--- Day_19.java
+++ Day_19.java
@@ -43,6 +43,12 @@ public class Day_19
     //     b[0] = 100;
     //     System.out.println("Inside Update "+b[0]);
     // }
+
+    // static int[] update()
+    // {
+    //     int a[] = {10,20,30,40};
+    //     return a;
+    // }
     public static void main(String [] args)
     {
         // Day_19 s = new Day_19();
@@ -53,12 +59,30 @@ public class Day_19
         // main_Salary();
 
         // int a[] = {10,20,30,40};
-
         // System.out.println("Befor Method Call " + a[0]);
-        
         // update(a); // pass by reference in java we don't have this toipc 
-
         // System.out.println("After Method Call " + a[0]);
+
+        // int b[] = update();
+
+        // System.out.print("Before Modification : ");
+        // for (int i = 0; i < b.length; i++)
+        // {
+        //     System.out.print(b[i] + " ");
+        // }
+        // // System.out.println(b[0] + " " + b[1] + " " + b[2] + " " + b[3] + " " );  
+
+        // b[0] = 100;
+
+        // System.out.println();
+
+        // System.out.print("After Modification : ");
+        // for(int j = 0; j < b.length; j++)
+        // {
+        //     System.out.print(b[j] + " ");
+        // }
+        // System.out.println(b[0] + " " + b[1] + " " + b[2] + " " + b[3] + " " );  
+
 
     }
 }
