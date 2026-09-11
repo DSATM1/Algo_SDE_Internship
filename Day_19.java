419860de07660ceaa1459cc292d3a5772cdfc841
Suraj <surajsppokale@gmail.com>
2026-09-11 11:45:57 +0530

    Comment out salary calculation methods in Day_19.java for future implementation

--- Day_19.java
+++ Day_19.java
@@ -10,39 +10,55 @@ public class Day_19
     //     System.out.println("Hello S P");
     // }
 
-    static void main_Salary()
-    {
-        Tax();
-        System.out.println("Final Salary Generated");
-    }
+    // static void main_Salary()
+    // {
+    //     Tax();
+    //     System.out.println("Final Salary Generated");
+    // }
 
-    static void Tax()
-    {
-        DA();
-        System.out.println("Tax Done");
-    }
+    // static void Tax()
+    // {
+    //     DA();
+    //     System.out.println("Tax Done");
+    // }
 
-    static void DA()
-    {
-        HRA();
-        System.out.println("DA Done");
-    }
+    // static void DA()
+    // {
+    //     HRA();
+    //     System.out.println("DA Done");
+    // }
 
-    static void HRA()
-    {
-        Basic();
-        System.out.println("HRA Dne");
-    }
-    static void Basic()
-    {
-        System.out.println("Basic Done");
-    }
+    // static void HRA()
+    // {
+    //     Basic();
+    //     System.out.println("HRA Dne");
+    // }
+    // static void Basic()
+    // {
+    //     System.out.println("Basic Done");
+    // }
+
+    // static void update(int b[] )
+    // {
+    //     b[0] = 100;
+    //     System.out.println("Inside Update "+b[0]);
+    // }
     public static void main(String [] args)
     {
         // Day_19 s = new Day_19();
         // s.swaroop();
         // s.suraj();
         // System.out.println("Hello");
-        main_Salary();
+        
+        // main_Salary();
+
+        // int a[] = {10,20,30,40};
+
+        // System.out.println("Befor Method Call " + a[0]);
+        
+        // update(a); // pass by reference in java we don't have this toipc 
+
+        // System.out.println("After Method Call " + a[0]);
+
     }
 }
