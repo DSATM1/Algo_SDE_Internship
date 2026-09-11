325d75f7a94a32260fcaff3b338d9349b9aee2b9
Suraj <surajsppokale@gmail.com>
2026-09-11 09:56:44 +0530

    Initial commit of Day_18.java with commented methods for even/odd checks

---
+++ Day_18.java
@@ -0,0 +1,57 @@
+// import java.util.*;
+
+public class Day_18
+{
+
+    // static void checkEvenOdd(int x)
+    // {
+    //     if (x % 2 == 0)
+    //     {
+    //         System.out.println("Even");
+    //     }
+    //     else
+    //     {
+    //         System.out.println("Odd");
+    //     }
+    // }
+
+    // static boolean EvenOdd(int n)
+    // {
+    //     return n % 2 == 0;
+    // }
+
+    // boolean isEven(int n)
+    // {
+    //     return n % 2 == 0;
+    // }
+
+    // void Even(int n)
+    // {
+    //     if (n % 2 == 0)
+    //     {
+    //         System.out.println("Even");
+    //     }
+    //     else
+    //     {
+    //         System.out.println("Odd");
+    //     }
+    // }
+    public static void main(String[] args)
+    {
+        // Scanner sc = new Scanner (System.in);
+        // checkEvenOdd(2);
+        // System.out.println("Hello");
+
+        // boolean res = EvenOdd(10);
+        // System.out.println(res);
+
+        // Day_18 sp = new Day_18();
+
+        // boolean result = sp.isEven(2);
+        // System.out.print(result);
+
+        // sp.Even(10);
+
+
+    }
+}
