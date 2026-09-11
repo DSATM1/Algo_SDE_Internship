58311b0b33967cf0d75e5cd47105b0d43743bef2
Suraj <surajsppokale@gmail.com>
2026-09-11 11:07:52 +0530

    Refactor Day_19.java to implement salary calculation methods

--- Day_19.java
+++ Day_19.java
@@ -10,11 +10,39 @@ public class Day_19
     //     System.out.println("Hello S P");
     // }
 
+    static void main_Salary()
+    {
+        Tax();
+        System.out.println("Final Salary Generated");
+    }
+
+    static void Tax()
+    {
+        DA();
+        System.out.println("Tax Done");
+    }
+
+    static void DA()
+    {
+        HRA();
+        System.out.println("DA Done");
+    }
+
+    static void HRA()
+    {
+        Basic();
+        System.out.println("HRA Dne");
+    }
+    static void Basic()
+    {
+        System.out.println("Basic Done");
+    }
     public static void main(String [] args)
     {
         // Day_19 s = new Day_19();
         // s.swaroop();
         // s.suraj();
         // System.out.println("Hello");
+        main_Salary();
     }
 }
