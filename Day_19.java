215c280872e1ee769134ff7fbdb90028353672ad
Suraj <surajsppokale@gmail.com>
2026-09-11 10:56:41 +0530

    Add Day_19.java with initial class structure and commented methods

---
+++ Day_19.java
@@ -0,0 +1,20 @@
+public class Day_19 
+{
+    // static void swaroop()
+    // {
+    //     System.out.println("Hi Soop");
+    // }
+
+    // static void suraj()
+    // {
+    //     System.out.println("Hello S P");
+    // }
+
+    public static void main(String [] args)
+    {
+        // Day_19 s = new Day_19();
+        // s.swaroop();
+        // s.suraj();
+        // System.out.println("Hello");
+    }
+}
