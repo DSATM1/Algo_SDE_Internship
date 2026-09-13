4b5574be0554c18f7012cf64f7d6f077027e390d
Suraj <surajsppokale@gmail.com>
2026-09-13 21:04:44 +0530

    Add GCD calculation implementation in Day19_Activity.java

---
+++ Day19_Activity.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Day19_Activity 
+{
+    void GCD()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First Number : ");
+        int num_a = sc.nextInt();
+        
+        System.out.print("Enter the Second Number : ");
+        int num_b = sc.nextInt();
+
+        int original_a = num_a;
+        int original_b = num_b;
+    
+    while (num_b > 0)
+    {
+        int common_div = num_a % num_b;
+        num_a = num_b;
+        num_b = common_div;
+    }
+    System.out.println("GCD of " + original_a + " and " + original_b + " is " + num_a);
+    sc.close();
+}
+    void main(String[] args)
+    {
+        GCD();
+    }
+}
