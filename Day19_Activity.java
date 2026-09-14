43a964938c03de57e19408cce1ed25a737f03c16
Suraj <surajsppokale@gmail.com>
2026-09-14 10:15:02 +0530

    Comment out GCD method implementation in Day19_Activity.java for future use

--- Day19_Activity.java
+++ Day19_Activity.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 public class Day19_Activity 
 {
-    void GCD()
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the First Number : ");
-        int num_a = sc.nextInt();
+    // void GCD()
+    // {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the Number : ");
+        // int num_a = sc.nextInt();
         
-        System.out.print("Enter the Second Number : ");
-        int num_b = sc.nextInt();
+    //     System.out.print("Enter the Second Number : ");
+    //     int num_b = sc.nextInt();
 
-        int original_a = num_a;
-        int original_b = num_b;
+    //     int original_a = num_a;
+    //     int original_b = num_b;
     
-    while (num_b > 0)
-    {
-        int common_div = num_a % num_b;
-        num_a = num_b;
-        num_b = common_div;
-    }
-    System.out.println("GCD of " + original_a + " and " + original_b + " is " + num_a);
-    sc.close();
-}
+    // while (num_b > 0)
+    // {
+    //     int common_div = num_a % num_b;
+    //     num_a = num_b;
+    //     num_b = common_div;
+    // }
+    // System.out.println("GCD of " + original_a + " and " + original_b + " is " + num_a);
+    // sc.close();
+    // }
     void main(String[] args)
     {
-        GCD();
+        // GCD();
     }
 }
