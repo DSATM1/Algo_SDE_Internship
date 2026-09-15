be397aa987b08f3739acd03fe4f83a7080680978
Suraj <surajsppokale@gmail.com>
2026-09-15 20:19:28 +0530

    Comment out the entire Tax_Slab class implementation

---
+++ Tax_Slab.java
@@ -0,0 +1,49 @@
+// import java.util.*;
+
+// public class Tax_Slab 
+// {
+//     static void income()
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter Your Income : ");
+//         long salary = sc.nextLong();
+
+//         if (salary > 0 && salary <= 50000)
+//         {
+//             System.out.println("0% Tax");
+//         }
+//         else if(salary >= 50001 && salary <= 100000)
+//         {
+//             System.out.println("5% Tax");
+//         }
+//         else if(salary >= 100001 && salary <= 300000)
+//         {
+//             System.out.println("10% Tax");
+//         }
+//         else if(salary >= 300001 && salary <= 500000)
+//         {
+//             System.out.println("15% Tax");
+//         }
+//         else if(salary >= 500001 && salary <= 700000)
+//         {
+//             System.out.println("20% Tax");
+//         }
+//         else if(salary >= 700001 && salary <= 1000000)
+//         {
+//             System.out.println("30% Tax");
+//         }
+//         else if (salary >= 1000001)
+//         {
+//             System.out.println("50% Tax");
+//         }
+//         else
+//         {
+//             System.out.println("Enter the Income Correctly");
+//         }
+//     }
+
+//     public static void main(String[] args)
+//     {
+//         income();
+//     }    
+// }
