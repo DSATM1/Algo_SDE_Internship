f20b62a8e28e3c3c5994e23a89216e2a7e5eb1f2
Suraj <surajsppokale@gmail.com>
2026-09-16 19:14:24 +0530

    Comment out the entire Vowels class implementation

--- Vowels.java
+++ Vowels.java
@@ -1,29 +1,28 @@
-import java.util.*;
+// import java.util.*;
 
-public class Vowels 
-{
-    static void english()
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Character :");
-        char ch = sc.charAt(0);
+// public class Vowels 
+// {
+//     static void english()
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the Character : ");
+//         String ch = sc.next();
 
-        char c[] = {'a','e','i','o','u'};
+//         char c[] = {'a','e','i','o','u'};
+//         char index = ch.charAt(0);
         
-        for (int i = 0; i <= c[i]; i++)
-        {
-            if (ch.charAt(0))
-            {
-                System.out.println("Character Is Vowel");
-            }
-            else
-            {
-                System.out.println("Character Is Consonant");
-            }
-        }
-    }
-    public static void main(String [] args) 
-    {
-        english();
-    }
-}
+//         for (int i = 0; i < c.length; i++)
+//         {
+//             if (index == c[i])
+//             {
+//                 System.out.println("Character Is Vowel");
+//                 return ;
+//             }
+//         }
+//         System.out.println("Character Is Consonant");
+//     }
+//     public static void main(String [] args) 
+//     {
+//         english();
+//     }
+// }
