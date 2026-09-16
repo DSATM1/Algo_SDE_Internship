61c23a2683e5a80fd1c50bd619390118eb058ea9
Suraj <surajsppokale@gmail.com>
2026-09-16 10:14:29 +0530

    Add Ascii class to print ASCII values of uppercase letters

---
+++ Ascii.java
@@ -0,0 +1,14 @@
+public class Ascii 
+{
+    static void asci_code()
+    {
+        for(char i = 65; i<= 90; i++)
+        {
+            System.out.println((int) i + " = " + i);
+        }
+    }
+    public static void main(String[] args)
+    {
+        asci_code();
+    }    
+}
