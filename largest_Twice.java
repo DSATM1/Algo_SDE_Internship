28283a56ade3f545e93ef9c96682379662922593
Suraj <surajsppokale@gmail.com>
2026-09-16 19:41:17 +0530

    Comment out the entire largest_Twice class implementation

---
+++ largest_Twice.java
@@ -0,0 +1,40 @@
+// import java.util.*;
+
+// public class largest_Twice 
+// {
+//     int largest()
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter three numbers :");
+//         int n1 = sc.nextInt();
+//         int n2 = sc.nextInt();
+//         int n3 = sc.nextInt();
+
+//         int result = max(n1, n2);
+//         result = max(result, n3);
+
+//         return result;
+//     }
+
+//     int max(int n1, int n2)
+//     {
+//         int result;
+
+//         if (n1 > n2)
+//         {
+//             result = n1; 
+//         }
+//         else 
+//         {
+//             result = n2;
+//         }
+//         return result ;
+//     }
+//     public static void main(String[] args)
+//     {
+//         largest_Twice obj = new largest_Twice();
+//         int result = obj.largest();
+
+//         System.out.println("Largest Number : " + result);
+//     }    
+// }
