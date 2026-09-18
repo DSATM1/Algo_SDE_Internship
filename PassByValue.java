c2d8c32dcf1ec39a68a67daf9b1375755d7dc34c
Suraj <surajsppokale@gmail.com>
2026-09-18 13:18:58 +0530

    Add PassByValue class to demonstrate pass by value concept with a get_name method

---
+++ PassByValue.java
@@ -0,0 +1,17 @@
+public class PassByValue 
+{
+    // Pass By ref 
+    public static String get_name1(get_name name)
+    {
+        name.name = name.name + " s p";
+        return name.name;
+    }
+    public static void main(String[] args)
+    {   
+        // String name = " ";
+        get_name obj = new get_name();
+        obj.name = "Suraj";
+        get_name1(obj);
+        System.out.println(obj.name);
+    }
+}
