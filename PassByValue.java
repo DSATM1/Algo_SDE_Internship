10567713f31c30e1a8b0d4216554eb86b66d20ed
Suraj <surajsppokale@gmail.com>
2026-09-19 01:22:08 +0530

    Refactor PassByValue class by commenting out unused get_name1 method and related code

--- PassByValue.java
+++ PassByValue.java
@@ -1,17 +1,27 @@
 public class PassByValue 
 {
     // Pass By ref 
-    public static String get_name1(get_name name)
+    // public static String get_name1(get_name name)
+    // {
+    //     name.name = name.name + "S P";
+    //     return name.name;
+    // }
+
+    public static int productNumber(int number)
     {
-        name.name = name.name + "S P";
-        return name.name;
+        return number * 2;
     }
     public static void main(String[] args)
     {   
         // String name = " ";
-        get_name obj = new get_name();
-        obj.name = "Suraj";
-        get_name1(obj);
-        System.out.println(obj.name);
+    //     get_name obj = new get_name();
+    //     obj.name = "Suraj";
+    //     get_name1(obj);
+    //     System.out.println(obj.name);
+
+    int number = 100;
+    int n2 = productNumber(number);  
+    System.out.println(n2);
+
     }
 }
