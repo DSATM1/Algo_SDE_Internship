0db92b0115e5b7fefff6d7c01d861aa4059ac6ba
Suraj <surajsppokale@gmail.com>
2026-09-19 19:15:15 +0530

    Even Numbers printing from 2 to 20 and commented the code

--- For_Loop.java
+++ For_Loop.java
@@ -2,10 +2,17 @@ public class For_Loop
 {
     public static void main(String [] args)
     {
-        // Print 1 to 10 
-        for ( int i = 1; i <= 10; i++)
-        {
-            System.out.println(i);
-        }
+        // // Print 1 to 10 
+        // for ( int i = 1; i <= 10; i++)
+        // {
+        //     System.out.println(i);
+        // }
+
+        // // Even Numbers
+        // for(int i = 2; i <= 20; i++)
+        // {
+        //     if(i % 2 == 0)
+        //     System.out.println(i);
+        // }
     }
 }
