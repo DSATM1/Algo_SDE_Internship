3982159a111e0a559fe6fad195c14c421f55c470
Suraj <surajsppokale@gmail.com>
2026-09-19 19:28:24 +0530

    Fix loop initialization in sum calculation in For_Loop class

--- For_Loop.java
+++ For_Loop.java
@@ -23,7 +23,7 @@ public class For_Loop
 
         // // Sum of Number from 1 to 10
         // int sum = 0 ;
-        // for ( int i = 0; i <= 10; i++)
+        // for ( int i = 1; i <= 10; i++)
         // {
         //     sum = sum+i;
         // }
