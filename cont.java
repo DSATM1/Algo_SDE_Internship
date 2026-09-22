e90cd319f5e5afdc775066de285cd8cad0eb1d8d
Suraj <surajsppokale@gmail.com>
2026-09-22 10:13:39 +0530

    Refactor loop structure in cont class to improve readability

--- cont.java
+++ cont.java
@@ -2,14 +2,13 @@ public class cont
 {
     public static void main(String[] args) 
     {
-    for (int i = 1; i<=5; i++)
-    {
-        if(i <= 3)
+        for (int i = 1; i<=5; i++)
         {
+            if(i <= 3)
+            {
             break;
+            }
+            System.out.println(i);
         }
-        System.out.println(i);
     }
-    
-}
 }
