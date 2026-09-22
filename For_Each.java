b3b2dc4b45c56b01956d4d8e1be06a9ef70aa5d4
Suraj <surajsppokale@gmail.com>
2026-09-22 20:53:57 +0530

    Comment out user input and array processing code in For_Each class

--- For_Each.java
+++ For_Each.java
@@ -4,27 +4,37 @@ public class For_Each
 {
     public static void main(String[] suraj)
     {
-        Scanner sc = new Scanner(System.in);
-        int arr[] = new int[5];
+        // Scanner sc = new Scanner(System.in);
+        // int arr[] = {1,2,3,4,5};//new int[5];
 
-        for(int i = 0; i<= arr.length-1; i++)
-        {
-            System.out.print("Enter your Array Elements for " + i + " Position : ");
-            arr[i] = sc.nextInt();
-        }
+        // for(int i = 0; i<= arr.length-1; i++)
+        // {
+        //     System.out.println(arr[i]);
+        //     // System.out.print("Enter your Array Elements for " + arr[i] + " Position : ");
+        //     // arr[i] = sc.nextInt();
+        // }
 
-        System.out.print("User Entered Array Elements : ");
+        // System.out.print("User Entered Array Elements : ");
 
-        // int c = 0;
-        for(int Ary : arr)
-        {
-            System.out.print(Ary);
+        // // int c = 0;
+        // for(int Ary : arr)
+        // {
+        //     System.out.print(Ary);
 
-            if(Ary < arr.length)
-            {
-                System.out.print(",");
-            }
-            Ary++;
-        }
+        //     if(Ary < arr.length)
+        //     {
+        //         System.out.print(",");
+        //     }
+        //     Ary++;
+        // }
+
+
+        // int arr[] = {10,20,30,40,50};
+        // int sum = 0;
+        // for (int i = 0; i<=arr.length; i++)
+        // {
+        //     sum = sum + arr[i];
+        // }
+        // System.out.println(sum);
     }
 }
