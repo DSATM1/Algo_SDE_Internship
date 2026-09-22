323429b83585aeffa922cc4958ac909918d18a61
Suraj <surajsppokale@gmail.com>
2026-09-22 11:23:57 +0530

    Implement traverse method to print elements of an array, excluding the value 3

--- Day_22.java
+++ Day_22.java
@@ -1,7 +1,28 @@
 public class Day_22 
 {
+    // For Each
+    static void traverse(int[] arr)
+    {
+        for(int x : arr)
+        {
+            if(x != 4-1)
+            System.out.print(x+",");
+        }
+    }
+
+    // Normal for loop
+    // static void show(int [] a)
+    // {
+    //     for (int i = 0; i < a.length; i++)
+    //     {
+    //         System.out.println(a[i]);
+    //     }
+    // }
     public static void main(String [] args)
     {
-        System.out.println("Hello");
+        // show(new int[] {1,2,3,4});
+        //traverse(new int[] {1,2,3,4}); // anonymous array which dosen't have a name  
+        int[ ] arr = {1,2,3,4};
+        traverse(arr);
     }
 }
