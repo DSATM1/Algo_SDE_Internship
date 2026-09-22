a17efc5e64acc256c98a4717155b640253f8da20
Suraj <surajsppokale@gmail.com>
2026-09-22 14:29:38 +0530

    Add Day22_Activity class with commented-out findAvg method and main method

---
+++ Day22_Activity.java
@@ -0,0 +1,20 @@
+public class Day22_Activity 
+{
+    // static int findAvg(int arr[])
+    // {
+    //     int sum = 0; 
+    //     for(int x : arr)
+    //     {
+    //         sum += x;  
+    //     }
+    //     int avg = sum/arr.length;
+
+    //     return avg;
+    // }
+    public static void main(String[] args)
+    {
+        // int arr[] = {1,2,3,4,5};
+        // System.out.print(findAvg(arr));
+    }
+    
+}
