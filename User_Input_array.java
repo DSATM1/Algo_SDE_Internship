804d91bbd504cfb13473db0803f0d3ebe48ead51
Suraj <surajsppokale@gmail.com>
2026-09-23 13:08:04 +0530

    Refactor User_Input_array class to improve output formatting and enhance array element display

--- User_Input_array.java
+++ User_Input_array.java
@@ -13,17 +13,27 @@ public class User_Input_array
     public static void main(String[] suraj)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array : ");
+        System.out.print("Enter the size of the array : ");
         int size = sc.nextInt();
 
         int arr[] = new int[size];
-        System.out.println("Enter " + size + " Elements : ");
+        System.out.print("Enter " + size + " Elements : ");
 
         for(int i = 0; i < size; i++)
         {
             arr[i] = sc.nextInt();
         }
-        System.out.println(arr);
+        System.out.print("Array Elements : ");
+        for (int x : arr)
+        {
+            System.out.print(x);
+            if(x != arr.length)
+            {
+                System.out.print(",");
+            }
+            
+        }
+
 
         // User_Input_array obj = new User_Input_array();
         // obj.userInput(arr);
