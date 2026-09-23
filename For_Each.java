0f3af4607ee3ffd99dec45cc6a5b8c1068e0c9d7
Suraj <surajsppokale@gmail.com>
2026-09-23 10:27:00 +0530

    Refactor input handling in For_Each class and clean up commented-out code

--- For_Each.java
+++ For_Each.java
@@ -4,29 +4,28 @@ public class For_Each
 {
     public static void main(String[] suraj)
     {
-        // Scanner sc = new Scanner(System.in);
-        // int arr[] = {1,2,3,4,5};//new int[5];
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
 
-        // for(int i = 0; i<= arr.length-1; i++)
-        // {
-        //     System.out.println(arr[i]);
-        //     // System.out.print("Enter your Array Elements for " + arr[i] + " Position : ");
-        //     // arr[i] = sc.nextInt();
-        // }
+        for(int i = 0; i < arr.length; i++)
+        {
+            // System.out.println(arr[i]);
+            System.out.print("Enter your Array Elements for " + i + " Position : ");
+            arr[i] = sc.nextInt();
+        }
 
-        // System.out.print("User Entered Array Elements : ");
+        System.out.print("User Entered Array Elements : ");
 
-        // // int c = 0;
-        // for(int Ary : arr)
-        // {
-        //     System.out.print(Ary);
+        // int c = 0;
+        for(int a : arr)
+        {
+            System.out.print(a);
 
-        //     if(Ary < arr.length)
-        //     {
-        //         System.out.print(",");
-        //     }
-        //     Ary++;
-        // }
+            if(a <= arr.length - 1)
+            {
+                System.out.print(",");
+            }
+        }
 
 
         // int arr[] = {10,20,30,40,50};
@@ -57,19 +56,19 @@ public class For_Each
         // }
         // System.out.println(max); 
 
-        int arr[] = {1,2,3,4,5};
-        int first_max = arr[0];
-        int sec_max = arr[0];
+        // int arr[] = {1,2,3,4,5};
+        // int first_max = arr[0];
+        // int sec_max = arr[0];
 
-        for(int num : arr)
-        {
-            if (num > first_max)
-            {
-                sec_max = first_max;
-                first_max = num;
-            }
-        }
-        System.out.println(sec_max);
+        // for(int num : arr)
+        // {
+        //     if (num > first_max)
+        //     {
+        //         sec_max = first_max;
+        //         first_max = num;
+        //     }
+        // }
+        // System.out.println(sec_max);
 
     }
 }
