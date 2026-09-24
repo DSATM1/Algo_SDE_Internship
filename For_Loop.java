443cdb8564195867f1895d2469a85afc84b43072
Suraj <surajsppokale@gmail.com>
2026-09-24 17:51:22 +0530

    Remove commented-out while loop for printing odd numbers in For_Loop class

--- For_Loop.java
+++ For_Loop.java
@@ -41,11 +41,13 @@ public class For_Loop
         //     System.out.println(i);
         // }
 
-        int i = 1;
-        while(i<=10)
-        {
-            System.out.println(i);
-            i+=2;
-        }
+        // int i = 1;
+        // while(i<=10)
+        // {
+        //     System.out.println(i);
+        //     i+=2;
+        // }
+
+        
     }
 }
