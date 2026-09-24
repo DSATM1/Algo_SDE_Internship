1baa14ea552f0bbc886961fcad333386d6d0fff8
Suraj <surajsppokale@gmail.com>
2026-09-24 17:59:48 +0530

    Add nested for loop to print a 3x3 grid of asterisks in For_Loop class

--- For_Loop.java
+++ For_Loop.java
@@ -48,6 +48,13 @@ public class For_Loop
         //     i+=2;
         // }
 
-        
+        for(int i = 1; i<= 3; i++)
+        {
+            for (int j = 1; j <= 3; j++)
+            {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
     }
 }
