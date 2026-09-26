fe0a9db43da43a8e69b879cb53e234035776ab64
Suraj <surajsppokale@gmail.com>
2026-09-26 07:38:24 +0530

    Add Algorithms class with main method to print greeting message

---
+++ Algorithms.java
@@ -0,0 +1,12 @@
+public class Algorithms
+{
+    public static void main(String[] args)
+    {
+        System.err.println("Hello Suraj S P");
+        System.err.println("Hello Suraj S P");
+        System.err.println("Hello Suraj S P");
+        System.err.println("Hello Suraj S P");
+        System.err.println("Hello Suraj S P");
+    }
+}
+
